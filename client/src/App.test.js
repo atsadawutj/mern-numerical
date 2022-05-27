83d305afaf5d2150712d4b1d40efd7d07fd6b9b2
Atsadawut Jaipakdee <68280031+Atsadawut13@users.noreply.github.com>
2022-05-27 15:39:49 +0700

    five commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-window.URL.createObjectURL = function() {};
-
 test('renders learn react link', () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
